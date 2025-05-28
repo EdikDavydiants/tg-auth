a3305b75ad69ba20172607a1b72a78be706d4680
Edik <davydiantsedik@mail.ru>
2025-05-28 18:26:41 +0300

    INITIAL

---
+++ src/main/java/com/telegram/webapp/TgAuthApplication.java
@@ -0,0 +1,14 @@
+package com.telegram.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgAuthApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/telegram/webapp/TgAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegram.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
