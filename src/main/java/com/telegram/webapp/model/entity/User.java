0b438b0115b46217d7997dc9a5a3133936f9aec4
Edik <davydiantsedik@mail.ru>
2025-05-28 19:15:39 +0300

    настройка liquibase

---
+++ src/main/java/com/telegram/webapp/model/entity/User.java
@@ -0,0 +1,25 @@
+package com.telegram.webapp.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+}
